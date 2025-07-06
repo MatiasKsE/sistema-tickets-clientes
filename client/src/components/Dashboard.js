b8bbcee49998bae48d0e9b6304cb15f86f3516f0
MatiasKsE <matias_kse@outlook.com>
2025-07-06 19:45:04 -0300

    Fix: asegurar importación de config en Dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import config from '../config';
 
 const Dashboard = () => {
   const [stats, setStats] = useState({
